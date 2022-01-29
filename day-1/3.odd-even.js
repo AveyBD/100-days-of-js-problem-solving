03257c8ea02fc4fa99c8bc66e156183f4f2cb5ef
Debashish D. Dev <hello@debashish.dev>
2022-01-29 20:17:44 +0330

    Odd Even using if else

---
+++ day-1/3.odd-even.js
@@ -0,0 +1,10 @@
+function oe(num) {
+    if (num % 2 == 0) {
+        console.log('The Number is Even');
+    } else {
+        console.log('The Number is Odd');
+    }
+
+}
+
+oe(2397831);
--- day-1/readme.md
+++ day-1/readme.md
@@ -1,2 +1,5 @@
+# Problems Name
+
 ## Check Leap Year Using JS. 
 ## Find Factorial using JS.
+## Find Odd Even of a given number. 
